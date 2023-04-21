5ce37ee56eff13eb1716d68a42ac632a1e650a85
Foni1019 <pakozdi.david@gmail.com>
2023-04-21 20:37:09 +0200

    Added a connection to a Deta database, now you can you can insert values and they will be stored

---
+++ database.py
@@ -0,0 +1,22 @@
+import os
+from deta import Deta
+import streamlit as st
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+DETA_KEY = os.getenv("DETA_KEY")
+
+deta = Deta(DETA_KEY)
+
+db = deta.Base("test")
+
+def insert_transaction(category, dateOfTransaction, amount, comment):
+        return db.put({"category": category, "dateOfTransaction": dateOfTransaction, "amount": amount, "comment": comment})
+
+def fetch_all():
+        res = db.fetch
+        return res.items
+
+
--- pages/1_Input_Data.py
+++ pages/1_Input_Data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import datetime
 from streamlit_option_menu import option_menu
+import database as db
 
 # ------ Settings ------
 incomes = ["Salary", "Family", "Scholarship", "Settlement", "Investing"]
@@ -25,12 +26,14 @@ with st.form("entry_form", clear_on_submit=True):
         category = st.selectbox("Category", incomes, key="category")
     else:
         category = st.selectbox("Category", expenses, key="category")
-    dateOfTransaction = st.date_input("Date", key="date")
+    dateOfTransaction = st.date_input("Date", key="dateOfTransaction")
     amount = st.number_input("Amount", min_value=0, format="%i", step=100, key = "amount")
     comment = st.text_area("Comment", placeholder="Enter a comment here ...")
     submitted = st.form_submit_button("Save Data")
     if submitted:
-        st.success("Data saved!")
+        db.insert_transaction(category, str(dateOfTransaction), amount, comment)
+        st.write("Data Saved!")
+
     
     
     
